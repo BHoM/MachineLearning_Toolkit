209f2f757544d9d344e987f2fed2cc4f9afb7b81
epignatelli <eduardo.pignatelli@burohappold.com>
2020-03-26 13:26:11 +0000

    initial setup

---
+++ MachineLearning_Engine/python/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='pyBHoM',
+    description='A Machine Learning Framework for the Buildings and Habitats object Model',
+    author='Eduardo Pignatelli',
+    author_email='info@bhom.xyz',
+    license='LGPLv3',
+    packages=["compute"],
+    version="3.1.0.0",
+    )
---
+++ MachineLearning_PostBuild/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using BH.Engine.Python;
+
+namespace BH.PostBuild.MachineLearning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string module;
+
+            // install pillow
+            module = "pillow";
+            Console.WriteLine($"Installing {module}...");
+            Compute.PipInstall(module);
+
+            // install pymongo
+            module = "pymongo";
+            Console.WriteLine($"Installing {module}...");
+            Compute.PipInstall(module);
+
+            // install numpy
+            module = "numpy";
+            Console.WriteLine($"Installing {module}...");
+            Compute.PipInstall(module);
+
+            // install matplotlib
+            module = "matplotlib";
+            Console.WriteLine($"Installing {module}...");
+            Compute.PipInstall(module);
+
+            // install pandas
+            module = "pandas";
+            Console.WriteLine($"Installing {module}...");
+            Compute.PipInstall(module);
+
+            // install scikit-learn
+            module = "scikit-learn";
+            Console.WriteLine($"Installing {module}...");
+            Compute.PipInstall(module);
+
+            // install tensorflow
+            module = "tensorflow";
+            Console.WriteLine($"Installing {module}...");
+            Compute.PipInstall(module, version: "2");
+
+            // install tensorflow
+            module = "jax";
+            Console.WriteLine($"Installing {module}...");
+            Compute.PipInstall(module);
+
+            // install pytorch
+            Console.WriteLine("Installing pytorch");
+            Compute.PipInstall("torch", version: "1.4.0", findLinks: "https://download.pytorch.org/whl/torch_stable.html");
+            Compute.PipInstall("torchvision", version: "0.5.0", findLinks: "https://download.pytorch.org/whl/torch_stable.html");
+
+            // install python bhom package
+            Console.WriteLine("Installing pyBHoM...");
+            Compute.RunCommand("cd ../../../python && pip install -e .");
+        }
+    }
+}
