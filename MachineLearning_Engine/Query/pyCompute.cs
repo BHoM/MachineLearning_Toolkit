1c5f0e2c6e9e28fcfd0974d3b3688178f530c132
epignatelli <eduardo.pignatelli@burohappold.com>
2020-04-02 17:12:42 +0100

    pyCompute and numpy as fields

---
+++ MachineLearning_Engine/Query/pyCompute.cs
@@ -0,0 +1,15 @@
+﻿using Python.Runtime;
+
+namespace BH.Engine.MachineLearning
+{
+    public static partial class Query
+    {
+        /*************************************/
+        /**** Public Fields              ****/
+        /*************************************/
+
+        public static PyObject pyCompute = Engine.Python.Query.Import("MachineLearning_Engine.Compute");
+
+        /*************************************/
+    }
+}
