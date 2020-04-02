c9c9fda5f1dab70061c7d557074d57510371c8ee
epignatelli <eduardo.pignatelli@burohappold.com>
2020-04-02 17:12:14 +0100

    adding tensor object

---
+++ MachineLearning_oM/Tensor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BH.oM.MachineLearning
+{
+    public class Tensor<T> where T : struct
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        public IntPtr Handle { get; set; } = new IntPtr();
+
+        /***************************************************/
+    }
+}
