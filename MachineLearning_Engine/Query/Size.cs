e531cdb3f1beb60449dcf0c64dfcfed10f825546
epignatelli <eduardo.pignatelli@burohappold.com>
2020-04-09 14:14:47 +0100

    use the convert function

--- MachineLearning_Engine/Query/Size.cs
+++ MachineLearning_Engine/Query/Size.cs
@@ -12,7 +12,7 @@ namespace BH.Engine.MachineLearning
 
         public static int Size(this Tensor tensor)
         {
-            return (int)(Compute.InvokeNumpy("size", tensor.ToNumpy())).As<int>();
+            return (int)(Compute.InvokeNumpy("size", tensor.ToNumpy())).FromPython<int>();
         }
 
         /*************************************/
