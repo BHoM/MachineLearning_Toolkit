974da262472b7a4ae68b02184f7ee8a15e3ae9a9
epignatelli <eduardo.pignatelli@burohappold.com>
2020-07-09 16:49:25 +0100

    trigger ci

--- MachineLearning_Engine/Compute/Invoke.cs
+++ MachineLearning_Engine/Compute/Invoke.cs
@@ -20,11 +20,8 @@
  * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
  */
 
-using BH.oM.Vision;
-using BH.oM.DeepLearning.Models;
 using Python.Runtime;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace BH.Engine.MachineLearning
 {
