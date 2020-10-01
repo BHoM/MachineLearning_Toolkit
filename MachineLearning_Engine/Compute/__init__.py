a4e5f63f2fb383a829c4d551d4ca415bd1433f66
epignatelli <eduardo.pignatelli@burohappold.com>
2020-10-01 09:30:26 +0100

    fiix impoort bug

--- MachineLearning_Engine/Compute/__init__.py
+++ MachineLearning_Engine/Compute/__init__.py
@@ -20,8 +20,6 @@
 # along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.
 #
 
-from . import LinearRegression
-
 from .Audio import PlayAudio
 from .Audio import SynthesiseSpeech
 from .Audio import RecogniseSpeech
